f4d6fd07d8de4e5cd5b6a8d9fdfb58eca493a1b7
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-04-24 19:32:43 +0530

    Login Page Basics Configured

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,7 +3,7 @@ import HeaderItem from "./Header";
 
 export default function Layout(){
     return(
-        <div>
+        <div className="p-4 flex flex-col min-h-screen">
             <HeaderItem/>
             <Outlet/>
         </div>
--- client/src/assets/pages/LoginPage.jsx
+++ client/src/assets/pages/LoginPage.jsx
@@ -1,3 +1,20 @@
-export default function LoginPage(){
-    return 'Login Page';
+import { Link } from "react-router-dom";
+
+export default function LoginPage() {
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form action="" className="max-w-md mx-auto">
+                    <input type="email" placeholder='cobsccomp@student.nibm.lk' />
+                    <input type="password" name="" id="" placeholder="password" />
+                    <button className="login">Login</button>
+                    <div className="text-center py-2 text-gray-500">Don't have an account yet ?
+                        <Link to={'/register'} className="underline text-black"> Register Now</Link>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    )
 }
