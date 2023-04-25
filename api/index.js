066a204c726782a93de5b588f7627e236046e6e3
Dimuth C Bandara <107378946+dimuthcbandara97@users.noreply.github.com>
2023-04-25 07:43:44 +0530

    Updated

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173/'
+// }))
+
+// app.get('/test',(req,res)=>{
+//     res.json('test ok')
+// })
+
+// app.listen(3000)
+
+const express = require("express");
+var cors = require('cors')
 const app = express();
-
-app.get('/test',(req,res)=>{
-    res.json('test ok')
-})
-
-app.listen(3000)
+app.use(cors());
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/test', createProxyMiddleware({ 
+    target: 'http://localhost:5173/', //original url
+    changeOrigin: true, 
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+app.listen(3000);
--- client/src/assets/pages/RegisterPage.jsx
+++ client/src/assets/pages/RegisterPage.jsx
@@ -1,18 +1,43 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function RegisterPage() {
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [password,setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    function registerUser(ev) {
+        ev.preventDefault()
+        // axios.get('http://localhost:3000/test')
+        axios.get('http://localhost:3000/test', //proxy uri
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                console.log(response);
+            });
+    }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form action="" className="max-w-md mx-auto">
-                    <input type="text" placeholder="Your Name Here"/>
-                    <input type="email" placeholder='cobsccomp@student.nibm.lk' />
-                    <input type="password" name="" id="" placeholder="password" />
+                <form action="" className="max-w-md mx-auto" onSubmit={registerUser}>
+                    <input type="text"
+                        placeholder="Your Name Here"
+                        value={name}
+                        onChange={ev => setName(ev.target.value)
+                        } />
+                    <input type="email"
+                        placeholder='cobsccomp@student.nibm.lk'
+                        value={email}
+                        onChange={ev => setEmail(ev.target.value)}
+                    />
+                    <input type="password" name="" id=""
+                        placeholder="password"
+                        value={password}
+                        onChange={ev => setPassword(ev.target.value)}
+                    />
                     <button className="login">Register</button>
                     <div className="text-center py-2 text-gray-500">Already have an account ?
                         <Link to={'/login'} className="underline text-black"> Sign In</Link>
